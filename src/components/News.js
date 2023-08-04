4ea78ae1b8cf8490c77beae96bc92e4d30e9ba2e
Aditya <adityakumarji74@gmail.com>
2023-08-04 23:33:29 +0530

    first commit

---
+++ src/components/News.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0
+    }
+  }
+
+  updateNews = async () => {
+    this.props.setProgress(10)
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
+    let data = await fetch(url);
+    this.props.setProgress(40)
+    let parsedData = await data.json();
+    this.props.setProgress(70)
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
+    })
+    this.props.setProgress(100)
+  }
+
+  capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false
+    })
+  }
+
+  async componentDidMount() {
+    this.updateNews()
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className='text-center'>Top Headlines - {this.capitalize(this.props.category)}</h1>
+        {this.state.loading && <Spinner/>}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return <div className="col-md-3" key={element.url}>
+                  <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} source={element.source.name} />
+                </div>
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    )
+  }
+}
+
+export default News
+
